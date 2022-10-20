54acc319d139e27d9eb12f27e6583b2b5cb34c3d
Nanda-Aung <aye81061@gmail.com>
2022-10-20 15:10:18 +0630

    provider

--- pages/management/providers/index.vue
+++ pages/management/providers/index.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="container-fluid">
-    <v-card>
-      <v-card-title>Providers</v-card-title>
-    </v-card>
+    <v-data-table :headers="providerHeaders" :items="providers">
+      <template #top>
+        <v-toolbar flat>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            hide-details
+          />
+          <v-divider vertical inset />
+          <v-spacer />
+          <createButton to="providers/create" />
+        </v-toolbar>
+      </template>
+    </v-data-table>
   </div>
 </template>
 <script>
+import createButton from '@/components/button/createButton.vue'
+import { providerHeaders } from '@/utils/tableHeaders'
 export default {
-  layout: 'dashboard'
-
+  components: { createButton },
+  layout: 'dashboard',
+  data: () => ({
+    providers: [],
+    search: '',
+    providerHeaders
+  }),
+  async fetch () {
+    console.log((await this.$axios.get('/providers')))
+    const { data, status, message } = (await this.$axios.get('/providers')).data
+    if (status === 1) {
+      this.providers = data
+    } else {
+      console.log(message)
+    }
+  }
 }
 </script>
--- utils/tableHeaders.js
+++ utils/tableHeaders.js
@@ -22,3 +22,36 @@ export const cityHeaders = [
     value: 'name'
   }
 ]
+
+export const providerHeaders = [
+  {
+    text: 'Name',
+    align: 'start',
+    value: 'name'
+  },
+  {
+    text: 'Phone',
+    align: 'start',
+    value: 'phone'
+  },
+  {
+    text: 'Email',
+    align: 'start',
+    value: 'email'
+  },
+  {
+    text: 'Address',
+    align: 'start',
+    value: 'address'
+  },
+  {
+    text: 'City',
+    align: 'start',
+    value: 'city.name'
+  },
+  {
+    text: 'Township',
+    align: 'start',
+    value: 'township.name'
+  }
+]
