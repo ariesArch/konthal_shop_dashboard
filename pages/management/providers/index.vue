34a98c701ecba18a2dac21ca59c01565daef6d8f
khinthuzaraung131 <kinthuzaraung36@gmail.com>
2022-10-28 23:17:46 +0630

    Provider List&Create

--- pages/management/providers/index.vue
+++ pages/management/providers/index.vue
@@ -1,13 +1,56 @@
 <template>
   <div class="container-fluid">
-    <v-card>
-      <v-card-title>Providers</v-card-title>
-    </v-card>
+    <v-data-table :headers="providerHeaders" :items="providers">
+      <template #top>
+        <v-toolbar flat>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            hide-details
+          />
+          <v-divider vertical inset />
+          <v-spacer />
+          <createButton to="providers/create" />
+        </v-toolbar>
+      </template>
+      <template #[`item.actions`]="{ item }">
+        <v-icon
+          class="mr-2"
+          color="info"
+          @click="$router.push(`/management/providers/${item.id}`)"
+        >
+          mdi-eye
+        </v-icon>
+        <v-icon
+          color="primary"
+          @click="$router.push(`/management/providers/${item.id}`)"
+        >
+          mdi-pencil
+        </v-icon>
+      </template>
+    </v-data-table>
   </div>
 </template>
 <script>
+import createButton from '@/components/button/createButton.vue'
+import { providerHeaders } from '@/utils/tableHeaders'
 export default {
-  layout: 'dashboard'
-
+  components: { createButton },
+  layout: 'dashboard',
+  data: () => ({
+    providers: [],
+    search: '',
+    providerHeaders
+  }),
+  async fetch () {
+    console.log(this.$axios.defaults)
+    const { data, status, message } = (await this.$axios.get('/providers')).data
+    if (status === 1) {
+      this.providers = data
+    } else {
+      console.log(message)
+    }
+  }
 }
 </script>
--- utils/tableHeaders.js
+++ utils/tableHeaders.js
@@ -50,6 +50,41 @@ export const branchHeaders = [
   { text: 'Actions', value: 'actions', sortable: false }
 
 ]
+
+export const providerHeaders = [
+  {
+    text: 'Name',
+    align: 'start',
+    value: 'name'
+  },
+  {
+    text: 'Phone',
+    align: 'start',
+    value: 'phone'
+  },
+  {
+    text: 'Email',
+    align: 'start',
+    value: 'email'
+  },
+  {
+    text: 'Address',
+    align: 'start',
+    value: 'address'
+  },
+  {
+    text: 'City',
+    align: 'start',
+    value: 'city.name'
+  },
+  {
+    text: 'Township',
+    align: 'start',
+    value: 'township.name'
+  },
+  { text: 'Actions', value: 'actions', sortable: false }
+
+]
 export const cityHeaders = [
   {
     text: 'Name',
