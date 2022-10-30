a8809dafb430d25a1615a708ae0b8b4ab30636d7
khinthuzaraung131 <kinthuzaraung36@gmail.com>
2022-10-30 14:06:14 +0630

    providerForm

--- pages/management/providers/index.vue
+++ pages/management/providers/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-    <v-data-table :headers="providerHeaders" :items="providers">
+    <v-data-table :headers="providerHeaders" :items="list">
       <template #top>
         <v-toolbar flat>
           <v-text-field
@@ -11,45 +11,80 @@
           />
           <v-divider vertical inset />
           <v-spacer />
-          <createButton to="providers/create" />
+          <!-- <createButton to="providers/create" /> -->
+          <v-btn color="primary" @click="showDialog('create')">
+            Create
+          </v-btn>
         </v-toolbar>
       </template>
       <template #[`item.actions`]="{ item }">
         <v-icon
           class="mr-2"
           color="info"
-          @click="$router.push(`/management/providers/${item.id}`)"
+          @click="showDialog('show',item)"
         >
           mdi-eye
         </v-icon>
         <v-icon
+          class="mr-2"
           color="primary"
-          @click="$router.push(`/management/providers/${item.id}`)"
+          @click="showDialog('edit',item)"
         >
           mdi-pencil
         </v-icon>
       </template>
     </v-data-table>
+    <providerForm
+      v-model="openFormDialog"
+      :title="dialogTitle"
+      :cities="cities"
+      :townships="townships"
+    />
+    <DetailDialog v-model="openDetailDialog" :item="selectedItem" title="Provider" />
   </div>
 </template>
 <script>
-import createButton from '@/components/button/createButton.vue'
+import localforage from 'localforage'
 import { providerHeaders } from '@/utils/tableHeaders'
+import providerForm from '@/components/FormDialog/providerForm'
 export default {
-  components: { createButton },
+  components: { providerForm },
   layout: 'dashboard',
   data: () => ({
-    providers: [],
+    cities: [],
+    townships: [],
+    list: [],
     search: '',
+    selectedItem: {
+
+    },
+    dialogTitle: '',
+    openDetailDialog: '',
+    openFormDialog: '',
     providerHeaders
   }),
   async fetch () {
-    console.log(this.$axios.defaults)
-    const { data, status, message } = (await this.$axios.get('/providers')).data
-    if (status === 1) {
-      this.providers = data
-    } else {
-      console.log(message)
+    await this.fetchList(this, '/providers')
+  },
+  async mounted () {
+    this.cities = await localforage.getItem('stored:cities')
+    this.townships = await localforage.getItem('stored:townships')
+  },
+  methods: {
+    showDialog (type, item = null) {
+      if (type === 'show') {
+        this.selectedItem = (({ name, name_mm, city, township }) => ({ name, name_mm, city_name: city.name, township_name: township.name }))(item)
+        this.openDetailDialog = !this.openDetailDialog
+      } else {
+        if (item !== null) {
+          this.selectedItem = (({ id, name, name_mm, city, township, phone, address, email }) => ({ id, name, name_mm, city_id: city.id, township_id: township.id, phone, address, email }))(item)
+          this.dialogTitle = 'Edit Provider'
+        } else {
+          this.dialogTitle = 'Create Provider'
+        }
+        this.$emit('openFormDialog', this.selectedItem)
+        this.openFormDialog = true
+      }
     }
   }
 }
